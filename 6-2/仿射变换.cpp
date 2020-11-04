a1d605e5bb9ad19d40ed062f3ec734b3f3e9b4d3
easy-fu <2416740760@qq.com>
2020-11-04 09:29:21 +0800

    .

---
+++ 6-2/仿射变换.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<opencv2/opencv.hpp>
+using namespace cv;
+int main()
+{
+	cv::Mat dstMat;
+	cv::Mat srcMat = cv::imread("C:/Users/DELL/Desktop/0.jpg", 1);
+	if (srcMat.empty()) return-1;
+	const cv::Point2f src_pt[] = {
+								   cv::Point2f(200,200),
+								   cv::Point2f(250,200),
+								   cv::Point2f(200,100),
+	};
+	const cv::Point2f dst_pt[] = {
+								   cv::Point2f(300,100),
+								   cv::Point2f(300,50),
+								   cv::Point2f(200,100),
+	};
+	const cv::Mat affine_matrix = cv::getAffineTransform(src_pt, dst_pt);
+	cv::warpAffine(srcMat, dstMat, affine_matrix, srcMat.size());
+	cv::imshow("src", srcMat);
+	cv::imshow("dst", dstMat);
+	cv::waitKey(0);
+}
